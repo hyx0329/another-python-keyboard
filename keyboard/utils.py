9f688336b926df797557595278ec0b55faf93fd9
hyx0329 <hyx0329@163.com>
2022-12-02 10:54:54 +0800

    comment out platform specific code

--- keyboard/utils.py
+++ keyboard/utils.py
@@ -8,11 +8,11 @@ def is_usb_connected():
 	if hasattr(supervisor.runtime, 'usb_connected'):
 		return supervisor.runtime.usb_connected
 
-	@micropython.asm_thumb
-	def mem(r0):
-		"""Read memory from the address"""
-		ldr(r0, [r0, 0])
-	return mem(0x40000438) == 0x3
+	#@micropython.asm_thumb
+	#def mem(r0):
+	#	"""Read memory from the address"""
+	#	ldr(r0, [r0, 0])
+	#return mem(0x40000438) == 0x3
 
 
 def do_nothing(*args, **kargs):
